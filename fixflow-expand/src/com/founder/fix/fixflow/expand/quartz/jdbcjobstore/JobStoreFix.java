682a2dac9e86d66a168e8c15bf8588bc872251a7
kenshinnet <kenshin.net@gmail.com>
2013-06-03 17:43:55 +0800

    修复了定时任务框架的的bug

--- fixflow-expand/src/com/founder/fix/fixflow/expand/quartz/jdbcjobstore/JobStoreFix.java
+++ fixflow-expand/src/com/founder/fix/fixflow/expand/quartz/jdbcjobstore/JobStoreFix.java
@@ -1,4 +1,3 @@
-
 package com.founder.fix.fixflow.expand.quartz.jdbcjobstore;
 
 import java.sql.Connection;
@@ -6,110 +5,114 @@ import java.sql.Connection;
 import org.quartz.JobPersistenceException;
 import org.quartz.impl.jdbcjobstore.JobStoreTX;
 
-
 import com.founder.fix.bpmn2extensions.coreconfig.QuartzConfig;
 import com.founder.fix.fixflow.core.impl.Context;
 import com.founder.fix.fixflow.core.impl.util.StringUtil;
 
-
 public class JobStoreFix extends JobStoreTX {
-	
+
 	@Override
-	protected Connection getConnection() throws JobPersistenceException{
-		
-		
-		
-		QuartzConfig quartzConfig=Context.getProcessEngineConfiguration().getQuartzConfig();
-		if(quartzConfig==null){
-			return null;
-		}
-		if(StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())){
-			if(Context.getDbConnection()==null){
-				return super.getConnection();
+	protected Connection getConnection() throws JobPersistenceException {
+
+		if (Context.getProcessEngineConfiguration() == null) {
+			return super.getConnection();
+		} else {
+			QuartzConfig quartzConfig = Context.getProcessEngineConfiguration()
+					.getQuartzConfig();
+			if (quartzConfig == null) {
+				return null;
 			}
-			else{
-				return Context.getDbConnection();
-			}
-		}else{
-			if(Context.getDbConnection(quartzConfig.getDataBaseId())==null){
-				return super.getConnection();
-			}
-			else{
-				return Context.getDbConnection(quartzConfig.getIsDefaultConfig());
+			if (StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())) {
+				if (Context.getDbConnection() == null) {
+					return super.getConnection();
+				} else {
+					return Context.getDbConnection();
+				}
+			} else {
+				if (Context.getDbConnection(quartzConfig.getDataBaseId()) == null) {
+					return super.getConnection();
+				} else {
+					return Context.getDbConnection(quartzConfig
+							.getIsDefaultConfig());
+				}
 			}
 		}
 
-		
-	
-		
 	}
 
 	@Override
 	protected void closeConnection(Connection arg0) {
-		
-		
 
-		QuartzConfig quartzConfig=Context.getProcessEngineConfiguration().getQuartzConfig();
-		if(quartzConfig==null){
-			return;
-		}
-		if(StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())){
-			if(Context.getDbConnection()==null){
-				super.closeConnection(arg0);
+		if (Context.getProcessEngineConfiguration() == null) {
+			super.closeConnection(arg0);
+
+		} else {
+			QuartzConfig quartzConfig = Context.getProcessEngineConfiguration()
+					.getQuartzConfig();
+			if (quartzConfig == null) {
+				return;
 			}
-		}else{
-			if(Context.getDbConnection(quartzConfig.getDataBaseId())==null){
-				super.closeConnection(arg0);
+			if (StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())) {
+				if (Context.getDbConnection() == null) {
+					super.closeConnection(arg0);
+				}
+			} else {
+				if (Context.getDbConnection(quartzConfig.getDataBaseId()) == null) {
+					super.closeConnection(arg0);
+				}
 			}
 		}
 
-		
-		
-		
-		
 	}
 
 	@Override
-	protected void commitConnection(Connection arg0) throws JobPersistenceException {
-		
-		
-		QuartzConfig quartzConfig=Context.getProcessEngineConfiguration().getQuartzConfig();
-		if(quartzConfig==null){
-			return;
-		}
-		if(StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())){
-			if(Context.getDbConnection()==null){
-				super.commitConnection(arg0);
+	protected void commitConnection(Connection arg0)
+			throws JobPersistenceException {
+
+		if (Context.getProcessEngineConfiguration() == null) {
+			super.commitConnection(arg0);
+		} else {
+
+			QuartzConfig quartzConfig = Context.getProcessEngineConfiguration()
+					.getQuartzConfig();
+			if (quartzConfig == null) {
+				return;
 			}
-		}else{
-			if(Context.getDbConnection(quartzConfig.getDataBaseId())==null){
-				super.commitConnection(arg0);
+			if (StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())) {
+				if (Context.getDbConnection() == null) {
+					super.commitConnection(arg0);
+				}
+			} else {
+				if (Context.getDbConnection(quartzConfig.getDataBaseId()) == null) {
+					super.commitConnection(arg0);
+				}
 			}
 		}
 
-
-		
 	}
 
 	@Override
 	protected void rollbackConnection(Connection arg0) {
-		
-		QuartzConfig quartzConfig=Context.getProcessEngineConfiguration().getQuartzConfig();
-		if(quartzConfig==null){
-			return;
-		}
-		if(StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())){
-			if(Context.getDbConnection()==null){
-				super.rollbackConnection(arg0);
+
+		if (Context.getProcessEngineConfiguration() == null) {
+			super.rollbackConnection(arg0);
+		} else {
+			QuartzConfig quartzConfig = Context.getProcessEngineConfiguration()
+					.getQuartzConfig();
+			if (quartzConfig == null) {
+				return;
 			}
-		}else{
-			if(Context.getDbConnection(quartzConfig.getDataBaseId())==null){
-				super.rollbackConnection(arg0);
+			if (StringUtil.getBoolean(quartzConfig.getIsDefaultConfig())) {
+				if (Context.getDbConnection() == null) {
+					super.rollbackConnection(arg0);
+				}
+			} else {
+				if (Context.getDbConnection(quartzConfig.getDataBaseId()) == null) {
+					super.rollbackConnection(arg0);
+				}
 			}
 		}
-		
-		
 
 	}
-   
+
 }
