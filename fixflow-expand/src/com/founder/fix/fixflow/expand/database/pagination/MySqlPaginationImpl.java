b1bc90bb3270af63d50732dba8bfbf272688930b
demonrain <demonrain@vip.qq.com>
2013-06-26 18:10:30 +0800

    添加了对MYSQL和DB2的分页实现类

--- fixflow-expand/src/com/founder/fix/fixflow/expand/database/pagination/MySqlPaginationImpl.java
+++ fixflow-expand/src/com/founder/fix/fixflow/expand/database/pagination/MySqlPaginationImpl.java
@@ -1,38 +1,69 @@
 package com.founder.fix.fixflow.expand.database.pagination;
 
 import com.founder.fix.fixflow.core.db.pagination.Pagination;
+import com.founder.fix.fixflow.core.impl.util.StringUtil;
 
 public class MySqlPaginationImpl implements Pagination {
+	protected static final String SQL_END_DELIMITER = ";";
 
 	public String getPaginationSql(String sql, int firstResult, int maxResults, String fields) {
-		// TODO Auto-generated method stub
-		return null;
+		sql = trim(sql);
+    	if(StringUtil.isEmpty(fields)){
+    		fields="*";
+    	}
+        
+    	StringBuffer sb = new StringBuffer(sql.length() + 20);
+    	sb.append("(");
+        sb.append("SELECT "+fields+" FROM (select A.* from(");
+        sb.append(sql);
+        sb.append(" )A");
+        sb.append(" )c LIMIT ");
+        sb.append(firstResult-1);
+        sb.append(",");
+        sb.append(maxResults-(firstResult-1));
+        sb.append(")");
+        
+        return sb.toString();
 	}
 
 	public String getIsNullLocalismSql(String sql) {
-		// TODO Auto-generated method stub
-		return null;
+		return "ISNULL("+sql+")";
 	}
 
 	public String getDateSql() {
-		// TODO Auto-generated method stub
-		return null;
+		return "datetime";
 	}
 
 	public String getCurrentDateSql() {
-		// TODO Auto-generated method stub
-		return null;
+		return "sysdate()";
 	}
 
 	public String getCastConvertSql(String sql, String type) {
-		// TODO Auto-generated method stub
-		return null;
+		return "convert("+sql+")";
 	}
 
 	public String getLocalismSql(String localismKey, String localismValue) {
-		// TODO Auto-generated method stub
+		if(localismKey.equals("processperformance")){
+			return "ISNULL(TIMEDIFF(start_time,end_time))";
+		}
+		
+		if(localismKey.equals("datediffconvert")) {
+			return "avg(TIMEDIFF(create_time,end_time))";
+		}
+		
+		if(localismKey.equals("datediffconvertorg")) {
+			return "avg(TIMEDIFF(x.create_time,x.end_time))";
+		}
+		
 		return null;
 	}
 
-
+	private String trim(String sql) {
+        sql = sql.trim();
+        if (sql.endsWith(SQL_END_DELIMITER)) {
+            sql = sql.substring(0, sql.length() - 1
+                    - SQL_END_DELIMITER.length());
+        }
+        return sql;
+    }
 }
