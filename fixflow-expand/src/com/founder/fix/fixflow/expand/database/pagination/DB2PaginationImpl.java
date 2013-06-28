02f0f7be43878358f681e9c4dcc61ee342dc1e78
kenshinnet <kenshin.net@gmail.com>
2013-06-28 09:57:31 +0800

    修改了分也类

--- fixflow-expand/src/com/founder/fix/fixflow/expand/database/pagination/DB2PaginationImpl.java
+++ fixflow-expand/src/com/founder/fix/fixflow/expand/database/pagination/DB2PaginationImpl.java
@@ -1,38 +1,73 @@
 package com.founder.fix.fixflow.expand.database.pagination;
 
+import com.founder.fix.apputil.util.StringUtil;
 import com.founder.fix.fixflow.core.db.pagination.Pagination;
 
 public class DB2PaginationImpl implements Pagination{
+	protected static final String SQL_END_DELIMITER = ";";
 
 	public String getPaginationSql(String sql, int firstResult, int maxResults, String fields) {
-		// TODO Auto-generated method stub
-		return null;
+		sql = trim(sql);
+    	String inFiled = fields;
+    	if(StringUtil.isNotEmpty(fields)){
+    		fields="*";
+    		inFiled="A.*";
+    	}
+        StringBuffer sb = new StringBuffer(sql.length() + 20);
+        sb.append("(");
+        sb.append("SELECT "+fields+" FROM (SELECT "+inFiled+", rownumber() over() as RN_ FROM (");
+        sb.append(sql);
+        sb.append(" )A");
+        sb.append(" )b WHERE b.RN_ <=");
+        sb.append(maxResults);
+        sb.append(" and b.RN_ >=");
+        if (firstResult >= 0) {
+            sb.append(firstResult);
+         } 
+        sb.append(")");
+        
+        return sb.toString();
 	}
 
 	public String getIsNullLocalismSql(String sql) {
-		// TODO Auto-generated method stub
-		return null;
+		return "ISNULL("+sql+")";
 	}
 
 	public String getDateSql() {
-		// TODO Auto-generated method stub
-		return null;
+		return "current date";
 	}
 
 	public String getCurrentDateSql() {
-		// TODO Auto-generated method stub
-		return null;
+		return "current timestamp";
 	}
 
 	public String getCastConvertSql(String sql, String type) {
-		// TODO Auto-generated method stub
-		return null;
+		return "cast("+sql+" as "+ type +")";
 	}
 
 	public String getLocalismSql(String localismKey, String localismValue) {
-		// TODO Auto-generated method stub
+
+		if(localismKey.equals("processperformance")){
+			return "ISNULL(end_time-start_time)";
+		}
+		
+		if(localismKey.equals("datediffconvert")) {
+			return "avg(end_time-create_time)";
+		}
+		
+		if(localismKey.equals("datediffconvertorg")) {
+			return "avg(x.end_time-x.create_time)";
+		}
+		
 		return null;
 	}
 
-
+	private String trim(String sql) {
+        sql = sql.trim();
+        if (sql.endsWith(SQL_END_DELIMITER)) {
+            sql = sql.substring(0, sql.length() - 1
+                    - SQL_END_DELIMITER.length());
+        }
+        return sql;
+    }
 }
