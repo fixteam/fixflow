a0e836d37c93fc1f4bfee76fa72413dfee4db553
yangchenhui <84989979@qq.com>
2013-07-19 17:23:54 +0800

    修改了以前遗留的测试类，保证能运行成功。

--- fixflow-test/src/java/com/founder/fix/fixflow/test/connection/ConnectionTest.java
+++ fixflow-test/src/java/com/founder/fix/fixflow/test/connection/ConnectionTest.java
@@ -8,10 +8,12 @@ import com.founder.fix.fixflow.test.AbstractFixFlowTestCase;
 public class ConnectionTest extends AbstractFixFlowTestCase {
 	
 	
-
+	/**
+	 * 测试获取数据库连接
+	 * @throws Exception
+	 */
 	public void testConnection() throws Exception
 	{
-		
 		assertNotNull(Context.getDbConnection());
 	}
 	
