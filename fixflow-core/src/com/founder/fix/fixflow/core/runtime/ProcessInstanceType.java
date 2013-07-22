f4e53d211bb54aa5f6dadaa58685061da4262e20
kenshinnet <kenshin.net@gmail.com>
2013-07-22 14:41:46 +0800

    新增了流程实例状态

---
+++ fixflow-core/src/com/founder/fix/fixflow/core/runtime/ProcessInstanceType.java
@@ -0,0 +1,26 @@
+package com.founder.fix.fixflow.core.runtime;
+
+/**
+ * 流程实例状态
+ * @author kenshin
+ *
+ */
+public enum ProcessInstanceType {
+	
+	
+	/**
+	 * 运行中
+	 */
+	RUNNING,
+	
+	/**
+	 * 正常结束
+	 */
+	COMPLETE,
+	/**
+	 * 终止
+	 */
+	TERMINATION
+	
+
+}
