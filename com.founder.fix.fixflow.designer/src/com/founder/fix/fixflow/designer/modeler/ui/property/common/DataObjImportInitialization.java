0e9ff628af8febe2c1aae070d614259797cd77d9
kenshinnet <kenshin.net@gmail.com>
2013-07-30 15:07:34 +0800

    去除了设计器代码

--- com.founder.fix.fixflow.designer/src/com/founder/fix/fixflow/designer/modeler/ui/property/common/DataObjImportInitialization.java
+++
@@ -1,9 +0,0 @@
-package com.founder.fix.fixflow.designer.modeler.ui.property.common;
-
-import java.util.List;
-
-public interface DataObjImportInitialization {
-	
-	List<DataObjImport> init();
-
-}
