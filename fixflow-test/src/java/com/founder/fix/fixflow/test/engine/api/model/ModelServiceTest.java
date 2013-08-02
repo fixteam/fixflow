d79ce689265e97aed051d816fa9784e88273883f
yangchenhui <84989979@qq.com>
2013-08-02 13:45:29 +0800

    engine测试类中开源声明的添加

--- fixflow-test/src/java/com/founder/fix/fixflow/test/engine/api/model/ModelServiceTest.java
+++ fixflow-test/src/java/com/founder/fix/fixflow/test/engine/api/model/ModelServiceTest.java
@@ -1,16 +1,34 @@
+/**
+ * Copyright 1996-2013 Founder International Co.,Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author yangchenhui
+ */
 package com.founder.fix.fixflow.test.engine.api.model;
 
 import java.util.List;
 import java.util.Map;
-
 import com.founder.fix.fixflow.core.impl.bpmn.behavior.ProcessDefinitionBehavior;
 import com.founder.fix.fixflow.core.impl.persistence.definition.ResourceEntity;
 import com.founder.fix.fixflow.core.model.DeploymentBuilder;
-import com.founder.fix.fixflow.core.model.DeploymentQuery;
 import com.founder.fix.fixflow.core.model.ProcessDefinitionQuery;
 import com.founder.fix.fixflow.test.AbstractFixFlowTestCase;
 import com.founder.fix.fixflow.test.Deployment;
 
+/**
+ * ModelService的测试类
+ */
 public class ModelServiceTest extends AbstractFixFlowTestCase {
 	
 	/**
@@ -27,8 +45,6 @@ public class ModelServiceTest extends AbstractFixFlowTestCase {
 		assertNotNull(processDefinitionBehavior);
 	}
 
-	
-	
 	/**
 	 * 测试流程定义发布
 	 */
