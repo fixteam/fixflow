61024b3b74e1488d9fc0401f078458c09100fa95
kenshinnet <kenshin.net@gmail.com>
2013-08-02 10:14:01 +0800

    添加了开源协议文件头信息
    Signed-off-by: kenshinnet <kenshin.net@gmail.com>

--- fixflow-core/src/com/founder/fix/fixflow/core/impl/log/OperationLog.java
+++ fixflow-core/src/com/founder/fix/fixflow/core/impl/log/OperationLog.java
@@ -1,28 +1,26 @@
 /**
- * Copyright c FOUNDER CORPORATION 2006 All Rights Reserved.
- * OperationLog.java
+ * Copyright 1996-2013 Founder International Co.,Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author kenshin
  */
 package com.founder.fix.fixflow.core.impl.log;
 
 import org.apache.log4j.Level;
 
 
-/**
- * [クラス名]<br>
- * 操作ログ出力クラス<br>
- * <br>
- * [機能概要]<br>
- * 操作ログ機能を提供するクラスを提供します。<br>
- * <br>
- * [変更履歴]<br>
- * 2007/02/28 ver1.00 新規作成 weifeng<br>
- * <br>
- * 2007/04/13 ver1.10 LogUtilクラスに結びつけをキャンセルします<br>
- * <br>
- * 
- * @author weifeng
- * @version 1.10
- */
+
 public class OperationLog {
 
     /**
