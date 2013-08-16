17a399741684b022aa8c972d7e5dcb55d3a5abcf
yangchenhui <84989979@qq.com>
2013-08-16 17:18:06 +0800

    增加了流程管控中心框架

---
+++ fixflow-manage-servlet/src/com/founder/fix/fixflow/FlowManager.java
@@ -0,0 +1,127 @@
+/**
+ *  Copyright 1996-2013 Founder International Co.,Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author yangchenhui
+ */
+package com.founder.fix.fixflow;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.founder.fix.fixflow.core.impl.util.StringUtil;
+import com.founder.fix.fixflow.service.FlowCenterService;
+import com.founder.fix.fixflow.service.ProcessDefinitionService;
+import com.founder.fix.fixflow.util.CurrentThread;
+import com.founder.fix.fixflow.util.SpringConfigLoadHelper;
+
+public class FlowManager extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("ISGET", "ISGET");
+		doPost(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CurrentThread.init();
+		ServletOutputStream out = null;
+		String action = StringUtil.getString(request.getParameter("action"));
+		if (StringUtil.isEmpty(action)) {
+			action = StringUtil.getString(request.getAttribute("action"));
+		}
+		RequestDispatcher rd = null;
+		try {
+			Map<String, Object> filter = new HashMap<String, Object>();
+
+			if (ServletFileUpload.isMultipartContent(request)) {
+				ServletFileUpload Uploader = new ServletFileUpload(
+						new DiskFileItemFactory());
+				// Uploader.setSizeMax("); // 设置最大文件尺寸
+				Uploader.setHeaderEncoding("utf-8");
+				List<FileItem> fileItems = Uploader.parseRequest(request);
+				for (FileItem item : fileItems) {
+					filter.put(item.getFieldName(), item);
+					if (item.getFieldName().equals("action"))
+						action = item.getString();
+				}
+			} else {
+				Enumeration enu = request.getParameterNames();
+				while (enu.hasMoreElements()) {
+					Object tmp = enu.nextElement();
+					Object obj = request
+							.getParameter(StringUtil.getString(tmp));
+
+//					if (request.getAttribute("ISGET") != null)
+						obj = new String(obj.toString().getBytes("ISO8859-1"),
+								"utf-8");
+
+					filter.put(StringUtil.getString(tmp), obj);
+				}
+			}
+
+			Enumeration attenums = request.getAttributeNames();
+			while (attenums.hasMoreElements()) {
+				String paramName = (String) attenums.nextElement();
+				Object paramValue = request.getAttribute(paramName);
+				// 形成键值对应的map
+				filter.put(paramName, paramValue);
+
+			}
+
+			String userId = StringUtil.getString(request.getSession()
+					.getAttribute(FlowCenterService.LOGIN_USER_ID));
+			filter.put("userId", userId);
+			request.setAttribute("nowAction", action);
+			if (action.equals("processDefinitionList")) {
+				ProcessDefinitionService processDefinitionService = (ProcessDefinitionService) SpringConfigLoadHelper.getBean("processDefinitionServiceImpl");
+				Map<String, Object> result = processDefinitionService.getProcessDefitionList(filter);
+				filter.putAll(result);
+				request.setAttribute("result", filter);
+				request.setAttribute("pageInfo", filter.get("pageInfo"));
+				rd = request.getRequestDispatcher("/manager/processDefinitionList.jsp");
+			}
+			
+			if (rd != null)
+				rd.forward(request, response);
+		}catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (out != null) {
+				out.flush();
+				out.close();
+			}
+			CurrentThread.clear();
+		}
+		
+	}
+	
+}
