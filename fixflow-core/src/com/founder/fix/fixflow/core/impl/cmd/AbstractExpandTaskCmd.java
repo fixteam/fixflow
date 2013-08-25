a9f9ff7d0f3157efdaf55dc9882ab7fd15023efc
kenshinnet <kenshin.net@gmail.com>
2013-08-25 18:29:42 +0800

    【core更新】AbstractExpandTaskCmd上增加了同意流程上下文加载方法,为统一cmd处理方法做准备。

--- fixflow-core/src/com/founder/fix/fixflow/core/impl/cmd/AbstractExpandTaskCmd.java
+++ fixflow-core/src/com/founder/fix/fixflow/core/impl/cmd/AbstractExpandTaskCmd.java
@@ -17,35 +17,44 @@
  */
 package com.founder.fix.fixflow.core.impl.cmd;
 
-
 import java.util.Map;
 
+import com.founder.fix.fixflow.core.exception.FixFlowBizException;
+import com.founder.fix.fixflow.core.exception.FixFlowException;
+import com.founder.fix.fixflow.core.factory.ProcessObjectFactory;
 import com.founder.fix.fixflow.core.impl.command.AbstractCustomExpandTaskCommand;
 import com.founder.fix.fixflow.core.impl.command.ExpandTaskCommand;
 import com.founder.fix.fixflow.core.impl.interceptor.Command;
+import com.founder.fix.fixflow.core.impl.interceptor.CommandContext;
+import com.founder.fix.fixflow.core.impl.persistence.ProcessInstanceManager;
+import com.founder.fix.fixflow.core.impl.persistence.TaskManager;
+import com.founder.fix.fixflow.core.impl.runtime.ProcessInstanceEntity;
+import com.founder.fix.fixflow.core.impl.runtime.TokenEntity;
+import com.founder.fix.fixflow.core.runtime.ExecutionContext;
+import com.founder.fix.fixflow.core.task.TaskInstance;
 
-public abstract class AbstractExpandTaskCmd<P extends AbstractCustomExpandTaskCommand,T>  implements Command<T>{
+public abstract class AbstractExpandTaskCmd<P extends AbstractCustomExpandTaskCommand, T> implements Command<T> {
 
 	/**
 	 * 任务编号
 	 */
 	protected String taskId;
-	
+
 	/**
 	 * 任务命令类型
 	 */
 	protected String commandType;
-	
+
 	/**
 	 * 用户命令编号
 	 */
 	protected String userCommandId;
-	
+
 	/**
 	 * 任务意见
 	 */
 	protected String taskComment;
-	
+
 	/**
 	 * 瞬态流程实例变量Map
 	 */
@@ -55,36 +64,71 @@ public abstract class AbstractExpandTaskCmd<P extends AbstractCustomExpandTaskCo
 	 * 持久化流程实例变量Map
 	 */
 	protected Map<String, Object> variables = null;
-	
-	protected ExpandTaskCommand expandTaskCommand=null;
-	
-	
-	protected String businessKey;
-	
-	protected String initiator;
-	
-	protected String processDefinitionKey;
-	
-	protected String agent;
-	
-	protected String admin;
-	
-	public AbstractExpandTaskCmd (P abstractCustomExpandTaskCommand){
-		
 
-		
-		this.taskId=abstractCustomExpandTaskCommand.getExpandTaskCommand().getTaskId();
-		this.commandType=abstractCustomExpandTaskCommand.getExpandTaskCommand().getCommandType();
-		this.userCommandId=abstractCustomExpandTaskCommand.getExpandTaskCommand().getUserCommandId();
-		this.taskComment=abstractCustomExpandTaskCommand.getExpandTaskCommand().getTaskComment();
+	protected ExpandTaskCommand expandTaskCommand = null;
+
+	protected String businessKey;
+
+	protected String initiator;
+
+	protected String processDefinitionKey;
+
+	protected String agent;
+
+	protected String admin;
+
+	public AbstractExpandTaskCmd(P abstractCustomExpandTaskCommand) {
+
+		this.taskId = abstractCustomExpandTaskCommand.getExpandTaskCommand().getTaskId();
+		this.commandType = abstractCustomExpandTaskCommand.getExpandTaskCommand().getCommandType();
+		this.userCommandId = abstractCustomExpandTaskCommand.getExpandTaskCommand().getUserCommandId();
+		this.taskComment = abstractCustomExpandTaskCommand.getExpandTaskCommand().getTaskComment();
 		this.transientVariables = abstractCustomExpandTaskCommand.getExpandTaskCommand().getTransientVariables();
 		this.variables = abstractCustomExpandTaskCommand.getExpandTaskCommand().getVariables();
-		this.expandTaskCommand=abstractCustomExpandTaskCommand.getExpandTaskCommand();
+		this.expandTaskCommand = abstractCustomExpandTaskCommand.getExpandTaskCommand();
+
+		this.initiator = abstractCustomExpandTaskCommand.getExpandTaskCommand().getInitiator();
+		this.processDefinitionKey = abstractCustomExpandTaskCommand.getExpandTaskCommand().getProcessDefinitionKey();
+		this.businessKey = abstractCustomExpandTaskCommand.getExpandTaskCommand().getBusinessKey();
+		this.agent = abstractCustomExpandTaskCommand.getExpandTaskCommand().getAgent();
+		this.admin = abstractCustomExpandTaskCommand.getExpandTaskCommand().getAdmin();
+	}
+
+	/**
+	 * 加载流程执行上下文对象
+	 */
+	protected ExecutionContext loadExecutionContext(CommandContext commandContext) {
+
+		if (this.taskId == null || this.taskId.equals("")) {
+			throw new FixFlowException("任务编号为空！");
+		}
+
+		// 获取任务管理器
+		TaskManager taskManager = commandContext.getTaskManager();
 		
-		this.initiator=abstractCustomExpandTaskCommand.getExpandTaskCommand().getInitiator();
-		this.processDefinitionKey=abstractCustomExpandTaskCommand.getExpandTaskCommand().getProcessDefinitionKey();
-		this.businessKey=abstractCustomExpandTaskCommand.getExpandTaskCommand().getBusinessKey();
-		this.agent=abstractCustomExpandTaskCommand.getExpandTaskCommand().getAgent();
-		this.admin=abstractCustomExpandTaskCommand.getExpandTaskCommand().getAdmin();
+		// 获取任务管理器
+		ProcessInstanceManager processInstanceManager = commandContext.getProcessInstanceManager();
+
+		// 根据指定id查询出任务的TO 不能做改变操作
+		TaskInstance taskInstanceQuery = taskManager.findTaskById(taskId);
+
+		if (taskInstanceQuery==null) {
+			throw new FixFlowBizException("未查询到指定的任务");
+		}
+		
+		String processInstanceId = taskInstanceQuery.getProcessInstanceId();
+		
+		String tokenId= taskInstanceQuery.getTokenId();
+		
+		ProcessInstanceEntity processInstance =processInstanceManager.findProcessInstanceById(processInstanceId);
+		
+		
+		TokenEntity tokenEntity=processInstance.getTokenMap().get(tokenId);
+		
+		//执行按钮表达式
+		ExecutionContext executionContext = ProcessObjectFactory.FACTORYINSTANCE.createExecutionContext(tokenEntity);
+		
+
+		return executionContext;
 	}
 }
