495c687d441e5407d53b7211d533e0b039e55ce7
yyx19870914 <80771676@qq.com>
2013-08-28 13:39:47 +0800

    新增检查按钮状态的方法

--- bpmcenter/WebContent/js/common.js
+++ bpmcenter/WebContent/js/common.js
@@ -9,7 +9,22 @@ Fix = {
 			if (r!=null) 
 				return unescape(r[2]); 
 			return null; 
-		}  
+		},
+		CheckBtnStatus:function(table){
+			var selectedRow = $("table.fix-table tr.selected").length;
+			if(selectedRow==0){
+				$("div[data-scope]").addClass("disable");
+				$("div[data-scope=all]").removeClass("disable");
+			}else if(selectedRow==1){
+				$("div[data-scope]").addClass("disable");
+				$("div[data-scope=all]").removeClass("disable");
+				$("div[data-scope=single]").removeClass("disable");
+			}else{
+				$("div[data-scope]").addClass("disable");
+				$("div[data-scope=all]").removeClass("disable");
+				$("div[data-scope=multi]").removeClass("disable");
+			}
+		}
 	},
 	OpenMethod:{
 		openWindow:function(url, width, height){
@@ -18,7 +33,5 @@ Fix = {
 			window.showModalDialog(url,{},"dialogWidth="+_width+"px;dialogHeight="+_height+"px");
 		}
 		
-	}
-		
-		
+	}	
 };
