200a2140d4a2e0ffcea2d190c51439ee2937470c
kenshinnet <kenshin.net@gmail.com>
2013-09-12 14:36:38 +0800

    【core更新】表名枚举增加了新的值

--- fixflow-core/src/com/founder/fix/fixflow/core/database/DataBaseTableEnum.java
+++ fixflow-core/src/com/founder/fix/fixflow/core/database/DataBaseTableEnum.java
@@ -12,6 +12,18 @@ public enum DataBaseTableEnum {
 	fixflow_his_token, 
 	fixflow_his_taskinstance, 
 	fixflow_his_taskidentitylink, 
-	fixflow_his_variable
+	fixflow_his_variable,
+	
+	
+	fixflow_def_deployment,
+	fixflow_def_bytearray,
+	fixflow_def_processdefinition,
+	
+
+	fixflow_agent_agentinfo,
+	fixflow_agent_agentdetails,
+	
+	
+	fixflow_mail
 
 }
