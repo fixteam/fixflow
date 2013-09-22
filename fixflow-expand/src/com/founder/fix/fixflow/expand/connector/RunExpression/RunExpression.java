56920f1c2eed1a37ace325457da4da2869e67b93
kenshinnet <kenshin.net@gmail.com>
2013-09-22 11:22:06 +0800

    【core、expand 更新】修复了执行表达式连接器的bug

--- fixflow-expand/src/com/founder/fix/fixflow/expand/connector/RunExpression/RunExpression.java
+++ fixflow-expand/src/com/founder/fix/fixflow/expand/connector/RunExpression/RunExpression.java
@@ -1,42 +1,23 @@
-/**
- * Copyright 1996-2013 Founder International Co.,Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * @author kenshin
- */
 package com.founder.fix.fixflow.expand.connector.RunExpression;
 
 
 import com.founder.fix.fixflow.core.runtime.ExecutionContext;
 import com.founder.fix.fixflow.core.action.ConnectorHandler;
-import com.founder.fix.fixflow.core.impl.expression.ExpressionMgmt;
 
 public class RunExpression implements ConnectorHandler {
 
-	private java.lang.String expressionText;
-	protected java.lang.Object outputObj;
+	private java.lang.Object expressionText;
+
 	public void execute(ExecutionContext executionContext) throws Exception {
-		Object object=ExpressionMgmt.execute(expressionText, executionContext);
-		outputObj=object;
+
 	}
 
-	public void  setExpressionText(java.lang.String expressionText){
+	public void  setExpressionText(java.lang.Object expressionText){
 		this.expressionText = expressionText;
 	}
 
 	public java.lang.Object  getOutputObj(){
-		return outputObj ;
+		return expressionText ;
 	}
 
 }
