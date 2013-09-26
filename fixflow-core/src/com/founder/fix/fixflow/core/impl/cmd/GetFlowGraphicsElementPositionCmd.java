fd1f6f46daa9c9e1240cd4ca52d5f88b6c5deda5
yangchenhui <84989979@qq.com>
2013-09-26 15:27:47 +0800

    修复流程图上折叠子流程标记错位的bug.

--- fixflow-core/src/com/founder/fix/fixflow/core/impl/cmd/GetFlowGraphicsElementPositionCmd.java
+++ fixflow-core/src/com/founder/fix/fixflow/core/impl/cmd/GetFlowGraphicsElementPositionCmd.java
@@ -17,13 +17,14 @@
  */
 package com.founder.fix.fixflow.core.impl.cmd;
 
-
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.bpmn2.BaseElement;
+import org.eclipse.bpmn2.FlowElement;
+import org.eclipse.bpmn2.SubProcess;
 import org.eclipse.bpmn2.di.BPMNDiagram;
 import org.eclipse.bpmn2.di.BPMNEdge;
 import org.eclipse.bpmn2.di.BPMNShape;
@@ -42,46 +43,36 @@ public class GetFlowGraphicsElementPositionCmd implements Command<Map<String, Ma
 	 * 流程定义编号
 	 */
 	protected String processDefinitionId;
-	
-	
 	protected DefinitionsBehavior definitions;
-
 	public GetFlowGraphicsElementPositionCmd(String processDefinitionId) {
-		
-		
 		this.processDefinitionId = processDefinitionId;
-		
-		
 	}
 
 	public Map<String, Map<String, Object>> execute(CommandContext commandContext) {
-
 		ProcessDefinitionManager processDefinitionManager = commandContext.getProcessDefinitionManager();
-		
 		ProcessDefinitionBehavior processDefinition = processDefinitionManager.findLatestProcessDefinitionById(processDefinitionId);
-		
 		DefinitionsBehavior definitions = processDefinition.getDefinitions();
 		Map<String, Map<String, Object>> positionInfo=new HashMap<String, Map<String,Object>>();
 		this.definitions=definitions;
 		List<BPMNDiagram> BPMNDiagramList = definitions.getDiagrams();
-		
+		Map<String,List<String>> tmpMap = new HashMap<String,List<String>>();
 		for (BPMNDiagram bpmnDiagram : BPMNDiagramList) {
-			
-			
 			for (DiagramElement diagramElement : bpmnDiagram.getPlane().getPlaneElement()) {
-				
 				if (diagramElement instanceof BPMNShape) {
 					BPMNShape bpmnShape = (BPMNShape) diagramElement;
 					Map<String, Object>  positionMap=new HashMap<String, Object>();
-					
 					BaseElement bpmnElement=getBaseElement(bpmnShape.getBpmnElement());
-				
-					
-					
 					if(bpmnElement==null){
 						continue;
 					}
-
+					//判断如果是折叠起来的子流程，则将其所有的子元素的坐标替换成父元素的
+					if(bpmnElement instanceof SubProcess){
+						if(bpmnShape.isIsExpanded() == false){
+							List<String> excludeNodeIds = null;
+							excludeNodeIds = getExcluesElement((SubProcess)bpmnElement);
+							tmpMap.put(bpmnElement.getId(), excludeNodeIds);
+						}
+					}
 					float x=bpmnShape.getBounds().getX();
 					float y=bpmnShape.getBounds().getY();
 					float height=bpmnShape.getBounds().getHeight();
@@ -95,28 +86,23 @@ public class GetFlowGraphicsElementPositionCmd implements Command<Map<String, Ma
 				}
 				if (diagramElement instanceof BPMNEdge) {
 					//BPMNEdge bpmnEdge = (BPMNEdge) diagramElement;
-					
 				}
 				
 			}
-			
-			
-			
 		}
-		
+		//判断如果是折叠起来的子流程，则将其所有的子元素的坐标替换成父元素的
+		for(String key : tmpMap.keySet()){
+			for(String nodeId:tmpMap.get(key)){
+				positionInfo.put(nodeId, positionInfo.get(key));
+			}
+		}
 		return positionInfo;
-		
-		//return CommonNodeToSVG(bpmnShape, new SvgCallActivityTo());
 	}
 
-	private  BaseElement getBaseElement(BaseElement baseElement)
-	{
-		
-		
+	private  BaseElement getBaseElement(BaseElement baseElement){
 		if(baseElement==null){
 			return null;
 		}
-		
 		if(baseElement.getId()==null){
 			BasicEObjectImpl basicEObjectImpl=(BasicEObjectImpl)baseElement;
 			if(basicEObjectImpl!=null&&basicEObjectImpl.eProxyURI()!=null){
@@ -130,12 +116,23 @@ public class GetFlowGraphicsElementPositionCmd implements Command<Map<String, Ma
 		}else{
 			return baseElement;
 		}
-		
-		
-		
 	}
 	
-	
-	
-
+	/**
+	 * 获取子流程的子元素
+	 * @param process
+	 * @return
+	 */
+	private List<String> getExcluesElement(SubProcess process){
+		List<String> nodeIds = new ArrayList<String>();
+		List<FlowElement> flowElements = process.getFlowElements();
+		for(FlowElement flow:flowElements){
+			if(flow instanceof SubProcess){
+				List<String> tmpNodeIds = getExcluesElement((SubProcess)flow);
+				nodeIds.addAll(tmpNodeIds);
+			}
+			nodeIds.add(flow.getId());
+		}
+		return nodeIds;
+	}
 }
