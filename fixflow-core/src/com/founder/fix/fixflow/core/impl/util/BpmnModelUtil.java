030db0c346e48046d3d33ef01fdba357dccbdd95
yangchenhui <84989979@qq.com>
2013-11-29 17:49:25 +0800

    封装了bpmnModelUtil.addExtensionElement()和bpmnModelUtil.addExtensionAttribute()

--- fixflow-core/src/com/founder/fix/fixflow/core/impl/util/BpmnModelUtil.java
+++ fixflow-core/src/com/founder/fix/fixflow/core/impl/util/BpmnModelUtil.java
@@ -8,13 +8,19 @@ import org.eclipse.bpmn2.Bpmn2Factory;
 import org.eclipse.bpmn2.Definitions;
 import org.eclipse.bpmn2.Documentation;
 import org.eclipse.bpmn2.Expression;
+import org.eclipse.bpmn2.ExtensionAttributeValue;
 import org.eclipse.bpmn2.FormalExpression;
 import org.eclipse.bpmn2.Process;
 import org.eclipse.bpmn2.di.BPMNEdge;
 import org.eclipse.bpmn2.di.BPMNShape;
 import org.eclipse.dd.di.DiagramElement;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.impl.BasicEObjectImpl;
+import org.eclipse.emf.ecore.impl.EStructuralFeatureImpl.SimpleFeatureMapEntry;
+import org.eclipse.emf.ecore.util.FeatureMap;
 
+import com.founder.fix.bpmn2extensions.fixflow.FixFlowPackage;
 import com.founder.fix.fixflow.core.impl.bpmn.behavior.DefinitionsBehavior;
 
 public class BpmnModelUtil {
@@ -29,6 +35,38 @@ public class BpmnModelUtil {
 			String documentationText=documentations.get(0).getText();
 			return documentationText;
 		}
+	}
+	
+	/**
+	 * 增加扩展元素
+	 * @param baseElement
+	 * @param eReference
+	 * @param o
+	 * @return
+	 */
+	public static boolean addExtensionElement(BaseElement baseElement,EReference eReference,Object o){
+		final FeatureMap.Entry extensionElementEntry = new SimpleFeatureMapEntry((org.eclipse.emf.ecore.EStructuralFeature.Internal) eReference, o);
+  	  	if(baseElement.getExtensionValues().size() > 0){
+  	  		baseElement.getExtensionValues().get(0).getValue().add(extensionElementEntry);
+  	  	}else{
+  	  		ExtensionAttributeValue extensionElement = Bpmn2Factory.eINSTANCE.createExtensionAttributeValue();
+  	  		extensionElement.getValue().add(extensionElementEntry);
+  	  		baseElement.getExtensionValues().add(extensionElement);
+  	  	} 
+		return false;
+	}
+	
+	/**
+	 * 增加擴展屬性
+	 * @param baseElement
+	 * @param eReference
+	 * @param o
+	 * @return
+	 */
+	public static boolean addExtensionAttribute(BaseElement baseElement,EAttribute eAttribute,Object o){
+		final FeatureMap.Entry extensionElementEntry = new SimpleFeatureMapEntry((org.eclipse.emf.ecore.EStructuralFeature.Internal) FixFlowPackage.Literals.DOCUMENT_ROOT__CATEGORY, o);
+		baseElement.getAnyAttribute().add(extensionElementEntry);
+		return false;
 	}
 	
 	public static void setDocumentation(BaseElement baseElement,String documentationText){
@@ -52,7 +90,6 @@ public class BpmnModelUtil {
 		}
 	}
 	
-	
 	public static String getExpression(Expression expression){
 		if(expression==null){
 			return null;
@@ -60,8 +97,6 @@ public class BpmnModelUtil {
 		return ((FormalExpression)expression).getBody();
 		
 	}
-	
-
 	
 	public static Expression getExpressionByString(String expression){
 		FormalExpression formalExpression=Bpmn2Factory.eINSTANCE.createFormalExpression();
@@ -72,8 +107,6 @@ public class BpmnModelUtil {
 	
 	public static BPMNShape getBpmnShape(Definitions definitions,String elementId){
 		List<DiagramElement> diagramElements=definitions.getDiagrams().get(0).getPlane().getPlaneElement();
-		
-		
 		for (DiagramElement diagramElement : diagramElements) {
 			if(diagramElement instanceof BPMNShape){
 				BPMNShape bpmnShape = (BPMNShape) diagramElement;
@@ -86,12 +119,7 @@ public class BpmnModelUtil {
 				}
 			}
 		}
-		
 		return null;
-		  
-		  
-		
-		
 	}
 	
 	public static  BaseElement getBaseElement(DefinitionsBehavior definitions,BaseElement baseElement){
@@ -115,60 +143,35 @@ public class BpmnModelUtil {
 	
 	public static BPMNEdge getBpmnEdge(Definitions definitions,String elementId){
 		List<DiagramElement> diagramElements=definitions.getDiagrams().get(0).getPlane().getPlaneElement();
-		
-		
 		for (DiagramElement diagramElement : diagramElements) {
 			if(diagramElement instanceof BPMNEdge){
 				String bpmnId=((BPMNEdge)diagramElement).getBpmnElement().getId();
 				if(elementId.equals(bpmnId)){
-			
 					return (BPMNEdge)diagramElement;
 				}
 			}
 		}
-		
 		return null;
-		  
-		  
-		
-		
 	}
 	
 	
 	public static BaseElement  getBaseElement(Definitions definitions,String elementId){
-		
-		
-		
 		return EMFUtil.findElement(elementId, definitions);
-
-		  
-		
-		
 	}
 	
 	public static <T> T  getElement(Definitions definitions,String elementId,Class<T> class1){
-		
-		
-		
 		return EMFUtil.findElement(elementId, definitions, class1);
-
 	}
 	
 	public static <T> List<T>  getElementList(BaseElement baseElement,Class<T> class1){
-		
-		
-		
 		return EMFUtil.getAll(class1, baseElement);
-
 	}
 	
 	public static Process getProcess(BaseElement baseElement){
-
 		return EMFUtil.getAll(Process.class, baseElement).get(0);
 	}
 	
 	public static List<Process> getProcessList(BaseElement baseElement){
-
 		return EMFUtil.getAll(Process.class, baseElement);
 	}
 
