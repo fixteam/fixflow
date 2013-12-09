745c10216c779209df31965baf569da9416723b1
yyx19870914 <80771676@qq.com>
2013-12-09 10:56:31 +0800

    人工任务默认值

--- fixflow-editor/WebContent/fixflow-editor/editor/oryx.debug.js
+++ fixflow-editor/WebContent/fixflow-editor/editor/oryx.debug.js
@@ -21201,10 +21201,12 @@ ORYX.Plugins.PropertyWindow = {
 				if(key == "oryx-overrideid" && firstShape.properties[key]==""){
 					var guid = Fix.Util.createGuid();
 					this.shapeSelection.commonPropertiesValues[key] = guid;
+					firstShape.properties[key] = guid;
 				}else if(key == "oryx-name" && firstShape.properties[key]==""){
 					var id = property._stencil._jsonStencil.id;
 					id = id.substring(id.indexOf("#")+1);
 					this.shapeSelection.commonPropertiesValues[key] = id;
+					firstShape.properties[key] = id;
 				}else{
 					this.shapeSelection.commonPropertiesValues[key] = firstShape.properties[key];
 				}
