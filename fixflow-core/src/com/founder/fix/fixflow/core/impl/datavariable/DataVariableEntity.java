54168b3a3416aba0ded41e8fed8e54ed69462828
kenshinnet <kenshin.net@gmail.com>
2013-12-23 19:42:40 +0800

    修改了数据变量的持久化

--- fixflow-core/src/com/founder/fix/fixflow/core/impl/datavariable/DataVariableEntity.java
+++ fixflow-core/src/com/founder/fix/fixflow/core/impl/datavariable/DataVariableEntity.java
@@ -18,8 +18,12 @@
 
 package com.founder.fix.fixflow.core.impl.datavariable;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -31,17 +35,30 @@ import com.founder.fix.fixflow.core.impl.expression.ExpressionMgmt;
 import com.founder.fix.fixflow.core.runtime.ExecutionContext;
 
 
-public class DataVariableEntity extends AbstractPersistentObject{
+public class DataVariableEntity extends AbstractPersistentObject<DataVariableEntity>{
 
+	
+	
+	
+	public static String QUERY_DATA_KEY="queryBizVariable";
+
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3906878781132663222L;
 	
-	public static String QUERY_DATA_KEY="queryBizVariable";
+	// 静态字段 //////////////////////////////////////////////////////////////
+	
+
+	public static final String RULE_GET_VARIABLE_PERSISTENT_STATE = "getVariablePersistentState";
+
+	public static final String RULE_GET_VARIABLE_PERSISTENT_DBMAP = "getVariablePersistentDbMap";
+
+	public static final String RULE_VARIABLE_CLONE = "variableClone";
 
 
-
+	// 需要持久化的字段 //////////////////////////////////////////////////////////
 	
 	protected String processInstanceId;
 
@@ -63,41 +80,20 @@ public class DataVariableEntity extends AbstractPersistentObject{
 	
 	protected boolean isPersistence;
 	
+	protected Date archiveTime;
 	
-	protected DataVariableBehavior dataVariableBehavior;
-
-	protected DataVariableMgmtInstance dataVariableMgmtInstance;
-	
-	public DataVariableEntity () {
-	
-	}
-
-
-	public DataVariableEntity (DataVariableBehavior dataVariableBehavior, DataVariableMgmtInstance dataVariableMgmtInstance) {
-	
-		
-		
-		
-		this.dataVariableBehavior = dataVariableBehavior;
-		this.isPersistence=dataVariableBehavior.isPersistence();
-		this.variableKey=dataVariableBehavior.getId();
-		this.dataVariableMgmtInstance = dataVariableMgmtInstance;
-		this.processInstanceId=dataVariableMgmtInstance.getProcessInstance().getId();
-		this.variableType=dataVariableBehavior.getBizType();
-
-	}
+	// get set //////////////////////////////////////////////////////////
 	
 	/**
-	 * 是否为持久化变量
-	 * @return
+	 * 获取变量编号
 	 */
-	public boolean isPersistence() {
-		return this.isPersistence;
-	}
-	public void setPersistence(boolean isPersistence) {
-		this.isPersistence = isPersistence;
+	public String getId() {
+
+		return this.variableKey;
 	}
 
+	
+	
 	public String getProcessInstanceId() {
 		return processInstanceId;
 	}
@@ -116,6 +112,12 @@ public class DataVariableEntity extends AbstractPersistentObject{
 
 	public byte[] getVariableValue() {
 		return variableValue;
+	}
+	
+	public Object getVariableObject() {
+		
+		
+		return bytesToObject(variableValue);
 	}
 
 	public void setVariableValue(byte[] variableValue) {
@@ -171,6 +173,56 @@ public class DataVariableEntity extends AbstractPersistentObject{
 	}
 	
 	
+	public Date getArchiveTime() {
+		return archiveTime;
+	}
+
+
+	public void setArchiveTime(Date archiveTime) {
+		this.archiveTime = archiveTime;
+	}
+
+	
+	
+	// 对象化元素 //////////////////////////////////////////////////////////
+
+	protected DataVariableBehavior dataVariableBehavior;
+
+	protected DataVariableMgmtInstance dataVariableMgmtInstance;
+	
+	public DataVariableEntity () {
+	
+	}
+
+
+	public DataVariableEntity (DataVariableBehavior dataVariableBehavior, DataVariableMgmtInstance dataVariableMgmtInstance) {
+	
+		
+		
+		
+		this.dataVariableBehavior = dataVariableBehavior;
+		this.isPersistence=dataVariableBehavior.isPersistence();
+		this.variableKey=dataVariableBehavior.getId();
+		this.dataVariableMgmtInstance = dataVariableMgmtInstance;
+		this.processInstanceId=dataVariableMgmtInstance.getProcessInstance().getId();
+		this.variableType=dataVariableBehavior.getBizType();
+
+	}
+	
+	/**
+	 * 是否为持久化变量
+	 * @return
+	 */
+	public boolean isPersistence() {
+		return this.isPersistence;
+	}
+	public void setPersistence(boolean isPersistence) {
+		this.isPersistence = isPersistence;
+	}
+
+	
+	
+	
 
 	public Object getExpressionValue() {
 		Object object = ExpressionMgmt.getVariable(getId());
@@ -181,38 +233,11 @@ public class DataVariableEntity extends AbstractPersistentObject{
 		ExpressionMgmt.setVariable(getId(), expressionValue);
 	}
 
-	public Map<String, Object> getPersistentState() {
 
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("PROCESSINSTANCE_ID", this.dataVariableMgmtInstance.getProcessInstance().getId());
-		map.put("VARIABLE_KEY", this.dataVariableBehavior.getId());
-		map.put("VARIABLE_VALUE", getExpressionValue());
-		map.put("VARIABLE_CLASSNAME", this.dataVariableBehavior.getDataType());
-		map.put("VARIABLE_CLASSNAME", this.dataVariableBehavior.getDataType());
-		map.put("VARIABLE_TYPE", this.dataVariableBehavior.getDataType());
-		return map;
-	}
 	
 
-	/**
-	 * 获取变量编号
-	 */
-	public String getId() {
+	
 
-		return this.variableKey;
-	}
-
-	@Override
-	public void persistentInit(Map<String, Object> entityMap) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Map<String, Object> getPersistentDbMap() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
 	public void executeExpression(ExecutionContext executionContext) {
 
@@ -255,6 +280,67 @@ public class DataVariableEntity extends AbstractPersistentObject{
 		}
 
 	}
+
+
+	@Override
+	public String getCloneRuleId() {
+		// TODO Auto-generated method stub
+		return RULE_VARIABLE_CLONE;
+	}
+
+
+	@Override
+	public String getPersistentDbMapRuleId() {
+		// TODO Auto-generated method stub
+		return RULE_GET_VARIABLE_PERSISTENT_DBMAP;
+	}
+
+
+	@Override
+	public String getPersistentStateRuleId() {
+		// TODO Auto-generated method stub
+		return RULE_GET_VARIABLE_PERSISTENT_STATE;
+	}
 	
+	
+	/**
+	 * byte[] to long
+	 * 
+	 * @param b
+	 * @return
+	 */
+	public static Object bytesToObject(byte[] b) {
+
+		if (b.length > 0) {
+			ObjectInput in = null;
+			try {
+				ByteArrayInputStream byteIn = new ByteArrayInputStream(b);
+				in = new ObjectInputStream(byteIn);
+				Object obj = in.readObject();
+
+				if (obj != null) {
+					return obj;
+				}
+
+			} catch (IOException e) {
+
+			} catch (ClassNotFoundException e) {
+
+			} finally {
+				if (in != null) {
+					try {
+						in.close();
+					} catch (IOException e) {
+
+					}
+				}
+			}
+
+			return null;
+		} else {
+
+			return null;
+		}
+	}
 
 }
