696dba22327ab4eae569dddb80f3687215b29b86
lccsuny <lccsuny@163.com>
2013-12-24 15:35:45 +0800

    新增一个方法，为单点登录使用。

--- bpmcenter/src/com/founder/fix/fixflow/LoginServlet.java
+++ bpmcenter/src/com/founder/fix/fixflow/LoginServlet.java
@@ -57,7 +57,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request,response);
+		//doPost(request,response);
 	}
 	
 	public void init(){
@@ -88,6 +88,11 @@ public class LoginServlet extends HttpServlet {
 				return;
 			}
 			//这里进行用户名密码验证，可以任意修改这里的代码。
+			String type = request.getParameter("type");
+			if ("singleLogin".equals(type)) {
+				singleLogin(request, response);
+				return;
+			}
 			list.add(userName);
 			password = MD5.getMD5(password.getBytes());
 			list.add(password);
@@ -149,5 +154,63 @@ public class LoginServlet extends HttpServlet {
 		}
 		
 	}
-
+	
+	private void singleLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DBConnFactory dbcf = (DBConnFactory)SpringConfigLoadHelper.getBean("DB_FIX_BIZ_BASE");
+		Connection connection = null;
+		List<Object> list= new ArrayList<Object>();
+		try {
+			String context = request.getContextPath();
+			String userName = request.getParameter("userName");
+			list.add(userName);
+			
+			boolean skip = false;
+			if(userName.equals("fixadmin")){
+				skip=true;
+			}
+			StringBuffer sb = new StringBuffer();
+			sb.append("select USERID,USERNAME,ISADMIN from AU_USERINFO where LOGINID=?");
+			connection = dbcf.createConnection();
+			//这里是自带的数据库操作方式。
+			SqlCommand sqlcommand = new SqlCommand(connection);
+			List<Map<String, Object>> list2 = sqlcommand.queryForList(sb.toString(),list);
+			if((list2!=null && list2.size()>0) || skip){
+				//这里约定了一个参数，流程引擎在运行时会默认从session里按照这两个key来获取参数，如果替换了登录的方式，请保证这两个key依然可以获取到正确的数据
+				if(skip){
+					request.getSession().setAttribute(FlowCenterService.LOGIN_USER_ID, "fixadmin");
+					request.getSession().setAttribute(FlowCenterService.LOGIN_USER_NAME, "跳过账号");
+					request.setAttribute("action", "getBPA");
+					response.sendRedirect(context+"/FixFlowBPACenter?action=getBPA");
+				}else{
+					request.getSession().setAttribute(FlowCenterService.LOGIN_USER_ID, list2.get(0).get("USERID"));
+					request.getSession().setAttribute(FlowCenterService.LOGIN_USER_NAME, list2.get(0).get("USERNAME"));
+					
+					//登录时根据登录的目标切换跳转目标
+					String loginType = request.getParameter("loginType");
+					if(loginType !=null && loginType.equals("on")){
+						request.setAttribute("action", "processDefinitionList");
+						response.sendRedirect(context+"/FlowManager?action=processDefinitionList");
+					}else{
+						request.setAttribute("action", "getBPA");
+						response.sendRedirect(context+"/FixFlowBPACenter?action=getBPA");
+					}
+					
+				}
+			}else{
+				response.setContentType("text/html;charset=utf-8");
+				response.getWriter().print("<script>alert('用户名不存在！');window.location.href='"+context+"/fixflow/login.jsp';</script>");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(connection!=null && !connection.isClosed())
+					connection.close();
+			} catch (SQLException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
